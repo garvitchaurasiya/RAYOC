3beb95696bd11fae9d3fff0af775a701ccc3d86c
Garvit Chaurasiya <garvitchaurasiya725@gmail.com>
2023-03-31 22:49:59 +0530

    Changed the class based component to function based

--- pages/post.js
+++ pages/post.js
@@ -1,67 +1,71 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Form, Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import web3 from '../ethereum/web3'
 import collegerating from '../ethereum/collegerating';
 import Layout from '../components/Layout';
-import {Router} from '../routes';
+import { useRouter } from 'next/router';
 
-export class post extends Component {
+function Post(){
+    const router = useRouter();
 
-    state = {
+    const [state, setState] = useState({
         studentName: '',
         collegeName: '',
         star: '',
         feedback: ''
-    }
+    });
 
-    onSubmit = async (event)=>{
+    const onSubmit = async (event)=>{
         event.preventDefault();
 
         const accounts = await web3.eth.getAccounts();
         await collegerating.methods.giveRating(
-            this.state.collegeName,
-            this.state.studentName,
-            this.state.star,
-            this.state.feedback
+            state.collegeName,
+            state.studentName,
+            state.star,
+            state.feedback
         ).send({
             from: accounts[0]
         })
         
-        Router.pushRoute('/');
+        router.pushRoute('/');
     }
 
-    onChange = (e)=>{
-        this.setState({...this.state , [e.target.name]: e.target.value})
+    const onChange = (e)=>{
+        setState({...state , [e.target.name]: e.target.value})
     }
+    return (
+        <Layout>
+            <Form onSubmit={onSubmit}>
+                <Form.Field>
+                    <label>Your Name</label>
+                    <input name="studentName" placeholder='Anonymous' onChange={onChange} value={state.studentName}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>College Name</label>
+                    <input name="collegeName" placeholder='Chitkara University' onChange={onChange} value={state.collegeName}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Star</label>
+                    <input name="star" placeholder='5' onChange={onChange} value={state.star}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Feedback</label>
+                    <textarea name="feedback" onChange={onChange} value={state.feedback}></textarea>
+                </Form.Field>
 
-    render() {
-        return (
-            <Layout>
-                <Form onSubmit={this.onSubmit}>
-                    <Form.Field>
-                        <label>Your Name</label>
-                        <input name="studentName" placeholder='Anonymous' onChange={this.onChange} value={this.state.studentName}/>
-                    </Form.Field>
-                    <Form.Field>
-                        <label>College Name</label>
-                        <input name="collegeName" placeholder='Chitkara University' onChange={this.onChange} value={this.state.collegeName}/>
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Star</label>
-                        <input name="star" placeholder='5' onChange={this.onChange} value={this.state.star}/>
-                    </Form.Field>
-                    <Form.Field>
-                        <label>Feedback</label>
-                        <textarea name="feedback" onChange={this.onChange} value={this.state.feedback}></textarea>
-                    </Form.Field>
+                <Button primary>Post</Button>
 
-                    <Button primary>Post</Button>
-
-                </Form>
-            </Layout>
-        )
-    }
+            </Form>
+        </Layout>
+    )
 }
+export default Post;
 
-export default post
+// export class post extends Component {
+    
+// }
+
+// export default post
+
