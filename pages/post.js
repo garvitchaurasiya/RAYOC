8e18f404e163e171de2095ca0130479d991ab081
Garvit Chaurasiya <garvitchaurasiya725@gmail.com>
2023-04-23 22:27:49 +0530

    Changed the UI completely.

--- pages/post.js
+++ pages/post.js
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import { Form, Button } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
+import React, { useState } from 'react'
 import web3 from '../ethereum/web3'
 import collegerating from '../ethereum/collegerating';
-import Layout from '../components/Layout';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 
-function Post() {
+export default function Post() {
     const [state, setState] = useState({
         studentName: '',
         collegeName: '',
-        star: '',
+        star: '5',
         feedback: ''
     });
     const router = useRouter();
     const [searchedColleges, setSearchedColleges] = useState([]);
     let allColleges = [];
     let searchTerm = "";
+    
 
     const filterContent = (searchTerm) => {
         let result = allColleges.filter((college) => college.name.toLowerCase().includes(searchTerm.toLowerCase()));
@@ -46,7 +43,6 @@ function Post() {
 
     const onSubmit = async (event) => {
         event.preventDefault();
-
         const accounts = await web3.eth.getAccounts();
         await collegerating.methods.giveRating(
             state.collegeName,
@@ -69,39 +65,169 @@ function Post() {
         input.value = name;
         document.getElementById("searchResults").style.display = "none";
     }
+    const checkedAnonymous = ()=>{
+        if(state.studentName === 'Anonymous'){
+            document.querySelector('#studentName').disabled = false;
+            setState({...state, studentName: ''})
+        }
+        else{
+            document.querySelector('#studentName').disabled = true;
+            setState({...state, studentName: 'Anonymous'})
+        }
+    }
     return (
-        <Layout>
-            <Form onSubmit={onSubmit}>
-                <Form.Field>
-                    <label>Your Name</label>
-                    <input name="studentName" placeholder='Anonymous' onChange={onChange} value={state.studentName} />
-                </Form.Field>
-                <Form.Field>
-                    <label>College Name</label>
-                    
-                    <input name="collegeName" id="searchBar" autoComplete="off" placeholder='Search' onChange={changeSearchTerm} />
+        <div className="flex flex-col justify-center items-center w-screen mt-4">
+            <div className='w-1/2'>
+                <form>
+                    <div className='w-full'>
+                        <div className="border-b border-gray-900/10 pb-12">
+                            <h2 className="text-base font-semibold leading-7 text-gray-900">Write a Feedback.</h2>
+                            <p className="text-sm leading-6 text-gray-600">Post a genuine feedback anonymously and keep in mind it can't be deleted.</p>
 
-                    <div id="searchResults" className='absolute bg-white border-2 w-full'>
-                        {searchedColleges.map((ele) => {
-                            return <div key={ele.rank} className="p-2 cursor-pointer hover:bg-gray-200" onClick={() => { onClickCollege(ele.name) }}>
-                                {ele.name}
+                            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-full">
+                                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Your Name
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            type="text"
+                                            id="studentName"
+                                            name="studentName"
+                                            placeholder='Name'
+                                            onChange={onChange}
+                                            value={state.studentName}
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                    <div className='flex'>
+                                        <div className="flex h-6 items-center">
+                                            <input
+                                            onClick={checkedAnonymous}
+                                            type="checkbox"
+                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                                            />
+                                        </div>
+                                        <div className="text-sm leading-6 ml-2">
+                                            <label htmlFor="candidates" className="font-medium text-gray-900">
+                                            Be Anonymous
+                                            </label>
+                                            <p className="text-gray-500">Check this box if don't want to show name.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="sm:col-span-full">
+                                    <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                                        College Name
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            name="collegeName"
+                                            id="searchBar"
+                                            autoComplete="off"
+                                            placeholder='Search'
+                                            onChange={changeSearchTerm}
+                                            className="block pl-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                        <div id="searchResults" className='w-1/2 rounded-md hidden absolute bg-white border-2'>
+                                        {searchedColleges.map((ele) => {
+                                            return <div key={ele.rank} className="p-2 cursor-pointer hover:bg-gray-200" onClick={() => { onClickCollege(ele.name) }}>
+                                                {ele.name}
+                                            </div>
+                                        })}
+                                    </div>
+                                    </div>
+                                </div>
+
+                                <div className="sm:col-span-full">
+                                    <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Rate Your College
+                                    </label>
+                                    <div className="mt-2">
+                                        <select
+                                        name="star"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        onChange={onChange}
+                                        >
+                                        <option value="5">Extremely good</option>
+                                        <option value="4">Good</option>
+                                        <option value="3">Neither good not bad</option>
+                                        <option value="2">Bad</option>
+                                        <option value="1">Extremely Bad</option>
+                                        </select>
+                                    </div>
+                                    <p className="text-sm text-gray-500">How would to rate this college to your juniors.</p>
+                                </div>
+
+                                <div className="col-span-full">
+                                    <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Feedback
+                                    </label>
+                                    <div className="mt-2">
+                                        <textarea
+                                            name="feedback"
+                                            onChange={onChange}
+                                            value={state.feedback}
+                                            rows={4}
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                    <p className="text-sm leading-6 text-gray-600">We would love to know how is your experience.</p>
+                                </div>
                             </div>
-                        })}
+                        </div>
                     </div>
-                </Form.Field>
-                <Form.Field>
-                    <label>Star</label>
-                    <input name="star" placeholder='5' onChange={onChange} value={state.star} />
-                </Form.Field>
-                <Form.Field>
-                    <label>Feedback</label>
-                    <textarea name="feedback" onChange={onChange} value={state.feedback}></textarea>
-                </Form.Field>
 
-                <Button primary>Post</Button>
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                        <button type="button" onClick={()=>{router.push('/')}} className="text-sm font-semibold leading-6 text-gray-900">
+                            Back
+                        </button>
+                        <button
+                            type="submit"
+                            onClick={onSubmit}
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Post
+                        </button>
+                    </div>
+                </form>
+            </div>
 
-            </Form>
-        </Layout>
+        </div>
     )
 }
-export default Post;
+
+//     return (
+//         <Layout>
+//             <Form onSubmit={onSubmit}>
+//                 
+//                 <Form.Field>
+//                     <label>College Name</label>
+                    
+//                     <input name="collegeName" id="searchBar" autoComplete="off" placeholder='Search' onChange={changeSearchTerm} />
+
+//                     <div id="searchResults" className='absolute bg-white border-2 w-full'>
+//                         {searchedColleges.map((ele) => {
+//                             return <div key={ele.rank} className="p-2 cursor-pointer hover:bg-gray-200" onClick={() => { onClickCollege(ele.name) }}>
+//                                 {ele.name}
+//                             </div>
+//                         })}
+//                     </div>
+//                 </Form.Field>
+//                 <Form.Field>
+//                     <label>Star</label>
+//                     <input name="star" placeholder='5' onChange={onChange} value={state.star} />
+//                 </Form.Field>
+//                 <Form.Field>
+//                     <label>Feedback</label>
+//                     <textarea name="feedback" onChange={onChange} value={state.feedback}></textarea>
+//                 </Form.Field>
+
+//                 <Button primary>Post</Button>
+
+//             </Form>
+//         </Layout>
+//     )
+// }
+// export default Post;
